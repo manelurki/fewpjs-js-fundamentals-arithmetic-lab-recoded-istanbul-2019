3ca45d7b042ef95517830e50b040ba25a5acb9d4
manel turki <menelturki@gmail.com>
2020-01-31 20:10:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const num1=2;
 const num2=31;
 let multiply=num1*num2;
 const random = Math.floor( Math.random() * 10) + 1
-const mod = 17%5;
-const max = Math.max( 1, 4, 7, 18 )
+const mod = 16%5;
+const max = Math.max( 1, 4, 20, 18 )
